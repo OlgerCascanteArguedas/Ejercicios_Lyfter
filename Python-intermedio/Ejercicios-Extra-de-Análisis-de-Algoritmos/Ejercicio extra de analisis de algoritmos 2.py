6581bc1b512fac2f3f06e8856eb2337fc4384693
Olger Cascante <olgerca100@gmail.com>
2026-05-15 14:10:21 -0600

    Respuesta ejercicios extra de analisis de algoritmos

---
+++ Python-intermedio/Ejercicios-Extra-de-Análisis-de-Algoritmos/Ejercicio extra de analisis de algoritmos 2.py
@@ -0,0 +1,9 @@
+#Ejercicio 1 O(N)
+#La lista no está ordenada.
+#La lista es pequeña.
+#No vale la pena ordenar la lista.
+#Ejercicio 2 O(log n)
+#La lista está ordenada.
+#La lista es grande.
+#Se van a hacer muchas búsquedas.
+#Binary search asume que los valores están ordenados. Si no lo están, puede buscar en la mitad equivocada y no encontrar un número aunque sí exista.
